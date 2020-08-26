c83eaed2b3c67c0ec139ac3fa0cab48b16603744
Cornelius Grieg Dahling <corneliusgdahling@gmail.com>
2020-08-26 20:33:52 +0200

    Invert hex color for value in chip and positon correctly

--- web-app/src/Chips.svelte
+++ web-app/src/Chips.svelte
@@ -1,48 +1,29 @@
 <script lang="ts">
 	import ChipIcon from './ChipIcon.svelte';
+	import { invertHex } from './invertHex';
 	export let chipValues;
 </script>
 
 <style>
-	svg {
-		position: absolute;
-		bottom: 44px;
-	}
-
-	/* .wrapper {
-		width: 200px;
-		height: 200px;
-	} */
-
 	.wrapper {
-		width: 100%;
-		display: flex;
-	}
-
-	.chipsWrapper {
-		display: flex;
-		margin: auto;
-	}
-
-	.chipContainer {
 		display: flex;
 		flex-direction: column;
 	}
 
-	.chipValue {
-		font-size: 40px;
+	.chipContainer {
+		margin: 20px;
 	}
 </style>
 
 <main>
 	<div class="wrapper">
-		<div class="chipsWrapper">
-			{#each Object.keys(chipValues) as chipColor, i}
-				<div class="chipContainer">
-					<ChipIcon color={chipColor} x={-i * 300} />
-					<span class="chipValue">{chipValues[chipColor]}</span>
-				</div>
-			{/each}
-		</div>
+		{#each Object.keys(chipValues) as chipColor, i}
+			<div class="chipContainer">
+				<ChipIcon
+					color={chipColor}
+					textColor={invertHex(chipColor)}
+					value={chipValues[chipColor]} />
+			</div>
+		{/each}
 	</div>
 </main>
