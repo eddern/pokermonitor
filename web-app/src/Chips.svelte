a106907d8e9f1faabafb4f6ca6339d79f1d7f532
Cornelius Grieg Dahling <corneliusgdahling@gmail.com>
2020-11-06 22:48:13 +0100

    Moved invertHex to Chips.Svelte

--- web-app/src/Chips.svelte
+++ web-app/src/Chips.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import ChipIcon from './ChipIcon.svelte';
-	import { invertHex } from './invertHex';
 	export let chipValues;
+
+	const invertHex = (hex: string) => {
+    return '#' + (Number(`0x1${hex.replace('#','')}`) ^ 0xFFFFFF).toString(16).substr(1).toUpperCase()
+  }
 </script>
 
 <style>
--- web-app/src/invertHex.js
+++
@@ -1,3 +0,0 @@
-export const invertHex = hex => {
-    return '#' + (Number(`0x1${hex.replace('#','')}`) ^ 0xFFFFFF).toString(16).substr(1).toUpperCase()
-  }
