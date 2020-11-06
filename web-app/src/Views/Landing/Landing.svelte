bee1fa3ebcd4f50292e38f196abfa74b20274a1e
eddern <edsc@netlight.com>
2020-11-06 21:58:02 +0100

    add basic landing page

--- web-app/src/Views/Landing/Landing.svelte
+++ web-app/src/Views/Landing/Landing.svelte
@@ -3,14 +3,31 @@
 </script>
 
 <style>
-	placeholder {
-		width: 100vw;
+	.container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.text-wrapper {
+		width: 50vw;
 		height: 50vh;
-		background: gold;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		background: #666;
 	}
 </style>
 
-<div>
-	<h1>Swag</h1>
-	<placeholder />
+<div class="container">
+	<div class="text-wrapper">
+		<h1>Welcome to pokermonitor</h1>
+		<h2>We are here for everyone</h2>
+		<h3>
+			Democrats, Liberals, all of the
+			<span style="color: red">United</span>
+			<span style="color: white">States</span>
+			<span style="color: black">of</span>
+			<span style="color: blue">America</span>
+		</h3>
+	</div>
 </div>
